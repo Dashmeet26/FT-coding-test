586c2d2f1abfca34820cdd0c3b3d7ad94f19bc27
Dashmeet Singh <dashmeet.s@gmail.com>
2019-11-25 09:18:17 +0530

    Initial commit with project structure

---
+++ FT-Coding-Test/FT-Coding-TestTests/FT_Coding_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  FT_Coding_TestTests.swift
+//  FT-Coding-TestTests
+//
+//  Created by Sridhar Agrawal on 25/11/19.
+//  Copyright © 2019 Dashmeet Singh. All rights reserved.
+//
+
+import XCTest
+@testable import FT_Coding_Test
+
+class FT_Coding_TestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
